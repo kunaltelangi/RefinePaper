eaa7c487f90eac51b09c15924248f8b02032f33c
Kunal Telangi <74617472+kunaltelangi@users.noreply.github.com>
2025-08-14 19:31:17 +0530

    Update paperfigure_parity_test.m

--- matlab/paperfigure_parity_test.m
+++ matlab/paperfigure_parity_test.m
@@ -1,37 +1,49 @@
+% Paths
 folder = 'latex-teaching/figures/';
-%LaTeX Defaults
+
+% LaTeX Defaults
 setlatex();
 
-%Single Parity Plot
+%% Single Parity Plot
 paperfigure();
+
+% Data setup
 ytrue = (0.01:0.0001:1).';
-ypred = normrnd(ytrue,0.02);
-parityplot(ytrue,ypred)
-savefigpng(folder,'single-parity')
-str = 'Parity plot with \\gls{rmse} of \\SI{%.5d}{\\J\\per\\square\\m}';
-caption = sprintf(str,get_errmetrics(ypred,ytrue,'rmse'));
-savefigstr(caption,'multi-parity',folder)
+ypred = normrnd(ytrue, 0.02);
 
-%Multiple Parity Plots
-ypred2 = normrnd(ytrue,0.05);
-ypred3 = normrnd(ytrue,0.1);
-ypred4 = normrnd(ytrue,0.15);
+% Plot & Save
+parityplot(ytrue, ypred);
+savefigpng(folder, 'single-parity');
 
-S2 = struct('ytrue',ytrue,'ypred',ypred2);
-S3 = struct('ytrue',ytrue,'ypred',ypred3);
-S4 = struct('ytrue',ytrue,'ypred',ypred4);
+% Caption
+rmse_single = get_errmetrics(ypred, ytrue, 'rmse');
+caption_str = sprintf('Parity plot with \\gls{rmse} of \\SI{%.5d}{\\J\\per\\square\\m}', rmse_single);
+savefigstr(caption_str, 'multi-parity', folder);
 
-Slist = {S,S2,S3,S4};
-multiparity(Slist,'ht',14)
-savefigpng(folder,'multi-parity')
-rmse = zeros(4,1);
-for i = 1:4
-    yp = Slist{i}.ypred;
-    rmse(i) = get_errmetrics(yp,ytrue,'rmse');
+%% Multiple Parity Plots
+% Predictions with different noise levels
+noise_levels = [0.02, 0.05, 0.1, 0.15];
+Slist = cell(1, numel(noise_levels));
+
+for idx = 1:numel(noise_levels)
+    Slist{idx} = struct( ...
+        'ytrue', ytrue, ...
+        'ypred', normrnd(ytrue, noise_levels(idx)) ...
+    );
 end
-writematrix(rmse,fullfile(folder,'parity-test-rmse.csv'))
 
-captionlist = get_captionlist(rmse);
-str = 'Parity plots with \\glspl{rmse} of %s \\SI{}{\\J\\per\\square\\m}.';
-caption = sprintf(str,captionlist);
-savefigstr(caption,'multi-parity',folder);
+% Plot & Save
+multiparity(Slist, 'ht', 14);
+savefigpng(folder, 'multi-parity');
+
+% RMSE calculation
+rmse_vals = zeros(numel(Slist), 1);
+for idx = 1:numel(Slist)
+    rmse_vals(idx) = get_errmetrics(Slist{idx}.ypred, ytrue, 'rmse');
+end
+writematrix(rmse_vals, fullfile(folder, 'parity-test-rmse.csv'));
+
+% Caption
+caption_list = get_captionlist(rmse_vals);
+caption_str = sprintf('Parity plots with \\glspl{rmse} of %s \\SI{}{\\J\\per\\square\\m}.', caption_list);
+savefigstr(caption_str, 'multi-parity', folder);
